7d35f8ae8d98b679be2c8f8db97f6127520451f5
Cordell <cordellsl@yahoo.com>
2019-05-31 13:27:08 -0400

    woking..

--- p-1.5_cssComb/parchild.js
+++ p-1.5_cssComb/parchild.js
@@ -30,29 +30,3 @@ btn2.addEventListener("click", () => {
         document.getElementById("Aerys").style.fontStyle = "normal";
     }
 });
-
-// let btn3 = document.getElementById("btn3");
-// let clicked3 = false;
-// btn3.addEventListener("click", () => {
-//     clicked3 = !clicked3;
-//     if(clicked3) {
-//         document.getElementById("swords").style.display = "none";
-//     }
-//     else {
-//         document.getElementById("swords").style.display = "block";
-//     }
-// });
-
-let btn3 = document.getElementById("btn3");
-let S = document.getElementById("swords");
-
-let clicked3 = false;
-btn3.addEventListener("click", () => {
-    clicked3 = !clicked3;
-    if(clicked3) {
-        S.style.display = "none";
-    }
-    else {
-        S.style.display = "block"
-    }
-});
