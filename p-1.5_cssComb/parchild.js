d582f8a99217fad9752e9ee951b0394d215f68a7
Cordell <cordellsl@yahoo.com>
2019-05-31 10:58:49 -0400

    final update?

--- p-1.5_cssComb/parchild.js
+++ p-1.5_cssComb/parchild.js
@@ -23,9 +23,36 @@ let clicked2 = false;
 btn2.addEventListener("click", () => {
     clicked2 = !clicked2;
     if(clicked2) {
-        document.getElementById("Aerys").style.fontStyle = "italic";
+        // document.getElementById("Aerys").style.fontStyle = "italic";
+        Ar.style.fontStyle = "italic";
     }
     else {
         document.getElementById("Aerys").style.fontStyle = "normal";
+    }
+});
+
+// let btn3 = document.getElementById("btn3");
+// let clicked3 = false;
+// btn3.addEventListener("click", () => {
+//     clicked3 = !clicked3;
+//     if(clicked3) {
+//         document.getElementById("swords").style.display = "none";
+//     }
+//     else {
+//         document.getElementById("swords").style.display = "block";
+//     }
+// });
+
+let btn3 = document.getElementById("btn3");
+let S = document.getElementById("swords");
+
+let clicked3 = false;
+btn3.addEventListener("click", () => {
+    clicked3 = !clicked3;
+    if(clicked3) {
+        S.style.display = "none";
+    }
+    else {
+        S.style.display = "block"
     }
 });
