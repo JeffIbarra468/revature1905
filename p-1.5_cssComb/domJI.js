89e636dceea6138acb329a6a9aa21f8205ac5731
Jeffrey Ibarra <jeffibarra468@gmail.com>
2019-05-31 12:34:53 -0700

    final

--- p-1.5_cssComb/domJI.js
+++ p-1.5_cssComb/domJI.js
@@ -11,6 +11,70 @@ btn.addEventListener("click", () => {
         document.getElementById("main-text").style.color = "black";
     }
 })
+
+/*
+let counter = 0;
+let HAS = document.getElementById("HAS");
+let ckd = false;
+HAS.addEventListener("click", () => {
+    ckd = !ckd;
+
+    if (ckd) {
+        // Next adj sib group
+        counter++;
+
+        switch(counter) {
+            case 1:
+                // Reset 
+                if((document.getElementById("BS").style.color != 'Black') && (document.getElementById("RkS").style.color != 'Black'))
+                 {  
+                    document.getElementById("BS").style.color = 'Black';
+                    document.getElementById("RkS").style.color = 'Black';
+                 }
+                else // BOLD RS & SS
+                 {
+                    document.getElementById("RS").style.color = 'Yellow';
+                    document.getElementById("SS").style.color = 'Yellow';
+                 }
+
+                if(counter === 4)
+                    counter = 1;
+                break;
+            case 2:
+                //BOLD RB & SS
+                document.getElementById("RS").style.color = 'Black';
+                document.getElementById("SS").style.color = 'Yellow';
+                document.getElementById("AS").style.color = 'Yellow';
+                
+                if(counter === 4)
+                    counter = 1;
+                break;
+            case 3:
+                //BOLD RB & SS
+                document.getElementById("SS").style.color = 'Black';
+                document.getElementById("AS").style.color = 'Yellow';
+                document.getElementById("BS").style.color = 'Yellow';
+                
+                if(counter === 4)
+                    counter = 1;
+                break;
+            case 4:
+                //BOLD RB & SS
+                document.getElementById("AS").style.color = 'Black';
+                document.getElementById("BS").style.color = 'Yellow';
+                document.getElementById("RkS").style.color = 'Yellow';
+
+                if(counter === 4)
+                    counter = 1;
+                break;
+            default:
+        }
+
+    } else {
+        // document.getElementById("main-text").style.color = "black";
+    }
+})
+*/
 /*
 // JS method for hovering links
 let hypLink = document.getElementById("hypLink");
